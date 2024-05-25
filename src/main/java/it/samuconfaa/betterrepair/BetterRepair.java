24c5532c767abbb2d4c82c108fa639ddbdea2d47
Samuconfaa <154546122+Samuconfaa@users.noreply.github.com>
2024-05-25 14:23:47 +0200

    Initial commit

---
+++ src/main/java/it/samuconfaa/betterrepair/BetterRepair.java
@@ -0,0 +1,17 @@
+package it.samuconfaa.betterrepair;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class BetterRepair extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
---
+++ src/main/java/it/samuconfaa/betterrepair/ConfigurationManager.java
@@ -0,0 +1,2 @@
+package it.samuconfaa.betterrepair;public class ConfigurationManager {
+}
